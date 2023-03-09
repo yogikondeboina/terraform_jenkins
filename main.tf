0c98854f2f86e96b1684657ca185d8d604344b2c
yogi <yogi@gmail.cOm>
2023-03-09 22:09:23 +0530

    add prod

--- main.tf
+++ main.tf
@@ -1,18 +1,20 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
 
-# Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-2"
 }
 
-# Create a VPC for ec2
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+module "vpc" {
+  source          = "./vpc"
+  vpc_cidr        = "10.0.0.0/16"
+  public_cidrs    = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_cidrs   = ["10.0.3.0/24", "10.0.4.0/24"]
+  transit_gateway = "${module.transit_gateway.transit_gateway}"
+}
+
+module "ec2" {
+  source         = "./ec2"
+  my_public_key  = "/tmp/id_rsa.pub"
+  instance_type  = "t2.micro"
+  security_group = "${module.vpc.security_group}"
+  subnets        = "${module.vpc.public_subnets}"
 }
